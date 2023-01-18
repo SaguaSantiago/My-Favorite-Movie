e0f0f064b75c21cb990b4834966ff3b6fb937737
SaguaSantiago <saguasantiago@gmail.com>
2023-01-18 14:27:42 -0300

    genres api

--- Components/Form/index.js
+++ Components/Form/index.js
@@ -6,10 +6,11 @@ import CustomTextField from 'Components/CustomComponents/CustomTextfield'
 import { StyledCheckbox } from 'Components/ServiceSelector/ServicesCheckbox/StyledComponents'
 
 import { useSelector, useDispatch } from 'react-redux'
-import { getAllMovies } from 'redux/reducers/movies'
+import { getAllMovies, toggleType } from 'redux/reducers/movies'
 
 import { Grid, Box, FormControlLabel, MenuItem, Button, Divider } from '@mui/material'
 import { getLanguagesRequest } from 'api/getLanguages'
+import GenresAccordion from 'Components/GenresAccordion'
 
 export default function Form() {
   const dispatch = useDispatch()
@@ -19,7 +20,7 @@ export default function Form() {
     Language: '',
     keywords: '',
   })
-  const { country, data } = useSelector((state) => state.movies)
+  const { country, data, type } = useSelector((state) => state.movies)
   useEffect(() => {
     getLanguagesRequest.then((res) =>
       setLenguages(res.sort((a, b) => a.english_name.localeCompare(b.english_name))),
@@ -33,19 +34,22 @@ export default function Form() {
       xs={12}
       item
     >
-      <Box mb={5} display='flex' justifyContent='center' gap={6}>
+      <Box display='flex' justifyContent='center' gap={6}>
         <FormControlLabel
           label='Movies'
-          checked={params.type.movie}
-          onChange={() => setParams({ ...params, type: 'movie' })}
+          checked={type === 'movie' ? true : false}
+          onChange={() => dispatch(toggleType('movie'))}
           control={<StyledCheckbox size='medium' sx={{ color: '#8888ffdd' }} />}
         />
         <FormControlLabel
           label='Series'
-          checked={params.type.serie}
-          onChange={() => setParams({ ...params, type: 'series' })}
+          checked={type === 'serie' ? true : false}
+          onChange={() => dispatch(toggleType('serie'))}
           control={<StyledCheckbox size='medium' sx={{ color: '#8888ffdd' }} />}
         />
+      </Box>
+      <Box margin='0 auto' mt={3} mb={5} width='80%'>
+        <GenresAccordion />
       </Box>
 
       <Grid container gap={3} alignItems='center' justifyContent='center'>
--- Components/GenresAccordion/index.js
+++ Components/GenresAccordion/index.js
@@ -5,42 +5,53 @@ import CustomAccordion from 'Components/CustomComponents/CustomAccordion'
 import { useDispatch, useSelector } from 'react-redux'
 import { addGenre, deleteGenre, addAvailableGenres } from 'redux/reducers/movies'
 
-import { getGenresForMovie } from 'api/getGenres'
+import { getGenresForMovie, getGenresForSeries } from 'api/getGenres'
 
 import { Chip } from '@mui/material'
 
 export default function GenresAccordion() {
-  const { availableGenres } = useSelector((state) => state.movies)
+  const { availableGenres, type } = useSelector((state) => state.movies)
   const dispatch = useDispatch()
   const genresSelected = useSelector((state) => state.movies.data.genresSelected)
 
   useEffect(() => {
-    getGenresForMovie.then((res) => {
-      const { genres } = res
-      dispatch(addAvailableGenres(genres))
-    })
-  }, [])
+    if (type === 'movie') {
+      getGenresForMovie.then((res) => {
+        const { genres } = res
+        dispatch(addAvailableGenres(genres))
+      })
+    } else {
+      getGenresForSeries.then((res) => {
+        const { genres } = res
+        dispatch(addAvailableGenres(genres))
+      })
+    }
+  }, [type])
 
   return (
     <CustomAccordion
       selectedItems={
         genresSelected.length === 0
           ? 'Genres'
-          : genresSelected.map(({ id, name }) => (
-              <Chip
-                key={id}
-                label={name}
-                sx={{ color: '#cccccc' }}
-                onClick={() => dispatch(deleteGenre(id))}
-                onDelete={() => dispatch(deleteGenre(id))}
-              />
-            ))
+          : genresSelected.map(({ id, name }) => {
+              if (availableGenres.some((g) => g.id === id)) {
+                return (
+                  <Chip
+                    key={id + Math.random() * 1000}
+                    label={name}
+                    sx={{ color: '#cccccc' }}
+                    onClick={() => dispatch(deleteGenre(id))}
+                    onDelete={() => dispatch(deleteGenre(id))}
+                  />
+                )
+              }
+            })
       }
       items={
         availableGenres &&
         availableGenres.map(({ id, name }) => (
           <Chip
-            key={id}
+            key={id + Math.random() * 1000}
             sx={{ color: '#cfcfcf' }}
             onClick={() => dispatch(addGenre({ id, name }))}
             label={name}
